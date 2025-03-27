b6a7b353e65ad54494caadf6c5d49787d8ba34c8
sporeball <sporeballdev@gmail.com>
2025-03-27 10:02:12 -0400

    fix user interaction bug in attribution

--- attribution.js
+++ attribution.js
@@ -1,4 +1,7 @@
-const main_containers = location.pathname === '/learn/'
+// import { play_sound } from "./learn/script.js";
+
+// containers to change the opacity for
+const oh_containers = location.pathname === '/learn/'
   ? [
     document.getElementById('back'),
     document.getElementById('options_container'),
@@ -7,29 +10,34 @@ const main_containers = location.pathname === '/learn/'
     document.getElementById('tasks_container'),
   ]
   : [document.getElementById('index_container')];
+// containers to disable user interaction for
+const in_containers = location.pathname === '/learn/'
+  ? [
+    document.getElementById('back'),
+    document.getElementById('options_container'),
+    document.getElementById('mutant_container'),
+    document.getElementById('image_container'),
+  ]
+  : [document.getElementById('index_container')];
 
 document.getElementById('attribution_a').onclick = function () {
-  if (location.pathname === '/learn/') {
-    play_sound('click');
-  }
+  // if (location.pathname === '/learn/') {
+  //   play_sound('click');
+  // }
   document.getElementById('attribution_container').className = '';
-  main_containers.forEach(c => {
-    c.classList.add('oh');
-    c.classList.add('in');
-  });
+  oh_containers.forEach(c => c.classList.add('oh'));
+  in_containers.forEach(c => c.classList.add('in'));
   document.getElementById('footer').className = 'oh in';
   document.body.className = 'attribution_open';
 }
 
 document.getElementById('close_attribution').onclick = function () {
-  if (location.pathname === '/learn/') {
-    play_sound('click');
-  }
+  // if (location.pathname === '/learn/') {
+  //   play_sound('click');
+  // }
   document.getElementById('attribution_container').className = 'dn';
-  main_containers.forEach(c => {
-    c.classList.remove('oh');
-    c.classList.remove('in');
-  });
+  oh_containers.forEach(c => c.classList.remove('oh'));
+  in_containers.forEach(c => c.classList.remove('in'));
   document.getElementById('footer').className = '';
   document.body.className = '';
 }
