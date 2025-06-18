869a1f0fa41ac3b31baa546036e502cd801cd3e6
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-06-18 22:08:36 +0530

    feat: implement Sentence class to extract sentences from lines of code

---
+++ scrape.js
@@ -0,0 +1,40 @@
+const fs = require('fs').promises;
+const tasks = await fs.readdir('./learn/tasks');
+
+class Sentence {
+  constructor(name, string) {
+    this.name = name;
+    this.string = string;
+    this.sentence = getSentenceFromString(string);
+    this.quote = null;
+  }
+
+  getSentenceFromString() {
+    const singleQuoteIndex = this.string.indexOf("'");
+    const doubleQuoteIndex = this.string.indexOf('"');
+
+    let startIndex;
+    if (singleQuoteIndex === -1 && doubleQuoteIndex === -1) {
+      // No quotes found
+      return null;
+    } else if (singleQuoteIndex === -1) {
+      startIndex = doubleQuoteIndex;
+      this.quote = '"';
+    } else if (doubleQuoteIndex === -1) {
+      startIndex = singleQuoteIndex;
+      this.quote = "'";
+    } else {
+      // Take the first occurring quote
+      startIndex = Math.min(singleQuoteIndex, doubleQuoteIndex);
+      this.quote = singleQuoteIndex < doubleQuoteIndex ? "'" : '"';
+    }
+
+    const endIndex = this.string.indexOf(this.quote, startIndex + 1);
+    if (endIndex === -1) {
+      // No closing quote found
+      return null;
+    }
+    this.sentence = this.string.substring(startIndex + 1, endIndex);
+    return this.sentence;
+  }
+}
