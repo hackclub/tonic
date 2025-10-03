f0b44c31786fee60b53b3fa509faad988c12514f
reemlifa <rkhalifa50@stuy.edu>
2025-10-03 13:27:16 -0400

    added submit button (doesn't redirect anywhere yet), added information to the gems and readme steps that didn't exist before

--- learn/tasks/all.js
+++ learn/tasks/all.js
@@ -1,10 +1,10 @@
 import github_setup from "./github_setup.js";
 import jekyll_setup from "./jekyll_setup.js";
 import your_first_page from "./your_first_page.js";
-import the_config_file from './the_config_file.js';
-import layouts from './layouts.js';
-import includes from './includes.js';
-import sass from './sass.js';
+import the_config_file from "./the_config_file.js";
+import layouts from "./layouts.js";
+import includes from "./includes.js";
+import sass from "./sass.js";
 import the_readme_file from "./the_readme_file.js";
 import gems from "./gems.js";
 import using_your_theme from "./using_your_theme.js";
@@ -22,4 +22,4 @@ export default {
   the_readme_file,
   gems,
   using_your_theme,
-}
+};
--- learn/tasks/using_your_theme.js
+++ learn/tasks/using_your_theme.js
@@ -1,12 +1,12 @@
-import Task from '../Task.js';
-import { mutant, hide_image, show_image } from '../script.js';
+import Task from "../Task.js";
+import { mutant, hide_image, show_image } from "../script.js";
 
 const using_your_theme = new Task({
-  name: 'Using your theme',
-  description: "Learn how to use your published theme!",
-  group: 'Show the world',
+  name: "Submit your theme!",
+  description: "Submit your theme and get a free sticker sheet!",
+  group: "Show the world",
 }).with_callback(async () => {
-  mutant.emote = 'slight_smile';
-})
+  mutant.emote = "slight_smile";
+});
 
 export default using_your_theme;
